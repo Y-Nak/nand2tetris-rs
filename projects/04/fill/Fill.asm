c48d12b3cf8703ddbbf76ac9c995a2404a24c23d
Y-Nak <yurayura.rounin.3@gmail.com>
2019-08-13 14:20:19 +0900

    Finish project04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,5 +10,63 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+@32
+D = A
+@row_word
+M = D
+@16
+D = A
+@col_word
+M = D
 
-// Put your code here.
+(MAINLOOP)
+    @i
+    M = 0
+    @SCREEN
+    D = A
+    @ptr
+    M = D
+    (OUTER)
+        @j
+        M = 0
+        (INNER)
+            @KBD
+            D = M
+            @FILLBLACK
+            D;JEQ
+            @color
+            M = 0
+            @FILL
+            0;JMP
+            (FILLBLACK)
+                @color
+                M = -1
+            (FILL)
+            @row_word
+            D = M
+            @j
+            D = D - M
+            @INNEREND
+            D;JEQ
+            @color
+            D = M
+            @ptr
+            A = M
+            M = D
+            @ptr
+            M = M + 1
+            @j
+            M = M + 1
+            @INNER
+            0;JMP
+        (INNEREND)
+        @col_word
+        D = M
+        @i
+        D = D - M
+        @MAINLOOP
+        D;JEQ
+        @i
+        M = M + 1;
+        @OUTER
+        0;JMP
